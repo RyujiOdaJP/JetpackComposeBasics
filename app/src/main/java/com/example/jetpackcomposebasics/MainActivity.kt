978b2312aca6cf6351232762ee69ba59e0b1a65c
Ryuji Oda <ryuji.oda@gmail.com>
2022-10-07 14:21:22 +0900

    lazyColumn

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -3,17 +3,12 @@ package com.example.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,48 +24,76 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 val names = listOf("Android", "Compose")
 
 @Composable
 fun MyApp(names: List<String>) {
+    // 状態をホイストします。ホイストは、状態にアクセスする必要がある共通の祖先に状態を移動
+    val shouldShowOnboarding = remember { mutableStateOf(true) }
+
     // A surface container using the 'background' color from the theme
     Surface(color = MaterialTheme.colors.background) {
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            names.forEach {
-                Greeting(name = it)
-            }
+        if (shouldShowOnboarding.value) OnboardingScreen {
+            shouldShowOnboarding.value = false
+        }
+        else Greetings()
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) {
+            Greeting(name = it)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             // weightをつけるとコンテンツが使用する領域を全て割り当てる、ついてないものは適度に寄せられる
-            Column(modifier = Modifier
-                .weight(1F)
-                .padding(bottom = extraPadding)
+            Column(
+                modifier = Modifier
+                    .weight(1F)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(
-                onClick = {
-                    expanded.value = !expanded.value
-                }
-            ) {
-                Text(text = if(expanded.value) "show more" else "show less")
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "show more" else "show less")
             }
         }
-        
+
     }
 
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { onContinueClicked() }
+            ) {
+                Text("Continue")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true, widthDp = 320)
@@ -78,5 +101,13 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     JetpackComposeBasicsTheme {
         MyApp(names)
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackComposeBasicsTheme {
+        OnboardingScreen {}
     }
 }
